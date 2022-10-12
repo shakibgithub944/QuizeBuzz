6e24fa364630824101c92f4a68498e585d2bb2f9
Nazmush Shakib <shakibgithub944>
2022-10-12 20:35:57 +0600

    question loaded

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
         <div>
             <Header></Header>
             <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-                <div className="grid gap-8 lg:grid-cols-3 sm:max-w-sm sm:mx-auto lg:max-w-full">
+                <div className="grid gap-8 lg:grid-cols-4 sm:max-w-sm sm:mx-auto lg:max-w-full">
                     {
                         topics.data.map(topic => <Topics
                             key={topic.id}
--- src/components/Quizes.js
+++ src/components/Quizes.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom'
+import ExamBoard from './ExamBoard';
 
 const Quizes = () => {
-    const quizes = useLoaderData();
-    // console.log(quizes.data.questions);
+    const quizes = useLoaderData().data.questions;
+    console.log(quizes);
     return (
         <div>
-            <h1>this is quiestions </h1>
+            {
+                quizes.map(question => <ExamBoard
+                key={question.id}
+                questions={question}
+                ></ExamBoard>)
+            }
+            
         </div>
     );
 };
